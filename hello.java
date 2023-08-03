4724a6b1a48e46d9e53f1ff52e384e7ef1fbec41
niresh121 <oggariniresh33@gmail.com>
2023-08-03 18:42:19 +0530

    hello tester1

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
 // Your First Program
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
+1.class HelloWorld { change1
+2.   public static void main(String[] args) {
+3.        System.out.println("Hello, World!"); 
+4.    }
